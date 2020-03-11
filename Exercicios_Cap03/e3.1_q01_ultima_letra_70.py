c87aa09fdfebde7624169f35c6003483550f71f4
Dnaja <61911770+Dnaja@users.noreply.github.com>
2020-03-10 22:10:08 -0300

    Update e3.1_q01_ultima_letra_70.py

--- Exercicios_Cap03/e3.1_q01_ultima_letra_70.py
+++ Exercicios_Cap03/e3.1_q01_ultima_letra_70.py
@@ -1,6 +1,20 @@
-def right_justify(s):
-    num_espaco = (70 - (len(s))) * " "
-    resultado = num_espaco + s
-    print(resultado)
+def main():
+    palavra = input("Digite uma palavra: ")
+    tam_linha = int(input("Digite o tamanho da linha: "))
 
-right_justify('monty')
+    resposta = right_justify(palavra, tam_linha)
+
+    print("linha formatada:\n{}".format(resposta))
+
+def right_justify(s, tam_espaco):
+    tam_palavra = len(s)
+
+    quant_espaco = tam_espaco - tam_palavra
+    espacos = quant_espaco * ' '
+    linha = espacos + s
+
+    return linha
+    
+
+main()
+
