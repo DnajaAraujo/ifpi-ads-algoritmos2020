0106f0c5ca42fbbb5489f18907d41a47f94cfd51
Dnaja <61911770+Dnaja@users.noreply.github.com>
2020-04-18 04:29:55 -0300

    Update f2_p1_q09_numero_primo.py

--- Fabio02/Fabio02_Parte01/f2_p1_q09_numero_primo.py
+++ Fabio02/Fabio02_Parte01/f2_p1_q09_numero_primo.py
@@ -6,7 +6,7 @@ def main():
 
 
 def verifica_numero_primo(n):
-    if (n == 1) or (n == 2) or (n % 2 == 0) or (n % 3 == 0) or (n % 5 == 0) or (n % 7 == 0):
+    if (n == 1) or (n != 2 and n % 2 == 0) or (n != 3 and n % 3 == 0) or (n != 5 and n % 5 == 0) or (n != 7 and n % 7 == 0):
         return f"{n} não é um número primo."
     else:
         return f"{n} é um número primo."
